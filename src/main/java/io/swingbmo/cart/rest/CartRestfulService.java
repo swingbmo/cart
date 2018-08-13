6fbb0222789ed96fe7b3278c16c6c66ae67f1f42
swingbmo <jomidos@gmail.com>
2018-08-13 22:30:26 +0800

    init a spirngboot web project

---
+++ src/main/java/io/swingbmo/cart/rest/CartRestfulService.java
@@ -0,0 +1,13 @@
+package io.swingbmo.cart.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CartRestfulService {
+
+	@GetMapping("/test")
+	public String test() {
+		return "hello spring boot!";
+	}
+}
