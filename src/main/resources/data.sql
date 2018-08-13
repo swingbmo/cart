24c60622dda0dc3d79c994c11f2b984dea18fe3a
swingbmo <jomidos@gmail.com>
2018-08-14 00:06:59 +0800

    add new data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,8 @@
 INSERT INTO cart_user(account, password, name, phone, address, email)
 VALUES('user123', 'pwd123', 'jack', '00-00-0000', 'address..', 'email@email.com');
+
+INSERT INTO cart_admin(account, password, name, email)
+VALUES('admin', 'admin', 'admin', 'email@email.com');
+
+INSERT INTO cart_info (title, desc, footerdesc)
+VALUES('HEADER TITLE', 'SITE DESCRIPTION', 'FOOTER DESCRIPTION');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,22 @@
+-- account
 CREATE TABLE cart_user (
 	account VARCHAR(25) primary key,
 	password VARCHAR(25) not null,
 	name VARCHAR(25) not null,
 	phone VARCHAR(25),
 	address VARCHAR(50),
-	email VARCHAR(50)
+	email VARCHAR(50) not null
+);
+CREATE TABLE cart_admin (
+	account VARCHAR(25) primary key,
+	password VARCHAR(25) not null,
+	name VARCHAR(25) not null,
+	email VARCHAR(50) not null
+);
+-- homepage
+CREATE TABLE cart_info (
+	id INTEGER auto_increment primary key,
+	title VARCHAR(25) not null,
+	desc VARCHAR(100),
+	footerdesc VARCHAR(100)
 );
